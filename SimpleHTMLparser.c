e3dcee8cd72fea02d8e1ab035ccf26a0d6a0d2f9
ip-is-not-an-id <110263241+ip-is-not-an-id@users.noreply.github.com>
2025-04-18 01:15:16 +0530

    Create SimpleHTMLparser.c

---
+++ SimpleHTMLparser.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+
+void parse (char html [100])
+
+{
+    printf("\n");
+    char cpy[100], mod[100];
+
+    int index, count;
+
+    count = 0;
+    while (html[count] != '>')
+    {
+        count++;
+    }
+
+    for (int i = 0; i < strlen(html); i++)
+    {
+        mod[i] = html[i + count + 1];
+    }
+
+    count = 0;
+    while (mod[count] != '<')
+    {
+        count++;
+    }
+
+    mod[count] = '\0';
+
+    count = 0;
+    while (mod[count] == ' ')
+    {
+        count++;
+    }
+
+    for (int i = 0; i < strlen(mod); i++)
+    {
+        cpy[i] = mod[i + count];
+    }
+
+    printf("%s\n", cpy);
+}
+
+int main()
+{
+    char htArr1[] = "<p> This a hypertext markup language file code 1 (p) </p>";
+    char htArr2[] = "<div> This a hypertext markup language file code 2 (div) </div>";
+    char htArr3[] = "<li> This a hypertext markup language file code 3 (li) </li>";
+    char htArr4[] = "<span> This a hypertext markup language file code 4 (span) </span>";
+    
+    parse (htArr1); 
+    parse (htArr2); 
+    parse (htArr3);
+    parse (htArr4);
+
+    return 0;
+}
